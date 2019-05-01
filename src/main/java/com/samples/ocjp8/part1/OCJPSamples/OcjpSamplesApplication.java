dc1c36afe2d373f83c187cb4cd47e3567c4ccf12
vxt3007 <vijaya_thatharola@homedepot.com>
2019-04-30 21:31:15 -0500

    Commit

---
+++ src/main/java/com/samples/ocjp8/part1/OCJPSamples/OcjpSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.samples.ocjp8.part1.OCJPSamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcjpSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcjpSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samples/ocjp8/part1/OCJPSamples/OcjpSamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samples.ocjp8.part1.OCJPSamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcjpSamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
