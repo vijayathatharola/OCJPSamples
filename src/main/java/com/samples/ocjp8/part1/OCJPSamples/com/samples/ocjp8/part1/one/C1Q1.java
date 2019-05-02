5c3f0e225b0496d5fc8e298c8898b153272d7182
vxt3007 <vijaya_thatharola@homedepot.com>
2019-05-02 15:34:45 -0500

    Commit

---
+++ src/main/java/com/samples/ocjp8/part1/OCJPSamples/com/samples/ocjp8/part1/one/C1Q1.java
@@ -0,0 +1,12 @@
+package com.samples.ocjp8.part1.OCJPSamples.com.samples.ocjp8.part1.one;
+
+public class C1Q1 extends Raptor{
+
+    public static void main(String[] args){
+
+        System.out.println("pre");
+        //new C1Q1();
+        System.out.println("hawk");
+    }
+
+}
